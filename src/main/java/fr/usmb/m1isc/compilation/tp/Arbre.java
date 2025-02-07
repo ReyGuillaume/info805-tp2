fd680ebe5a66255edc03974ec3e81fae1c5dac95
Guillaume Rey <grey02@orange.fr>
2025-02-07 09:23:07 +0100

    feat: enumerate types

--- src/main/java/fr/usmb/m1isc/compilation/tp/Arbre.java
+++ src/main/java/fr/usmb/m1isc/compilation/tp/Arbre.java
@@ -5,6 +5,28 @@ public class Arbre {
     public Arbre fd;
     public String value;
 
+    public static final String LET = "LET";
+    public static final String WHILE = "WHILE";
+    public static final String IF = "IF";
+    public static final String THEN = "THEN";
+    public static final String NULL = "null";
+    public static final String INPUT = "INPUT";
+    public static final String OUTPUT = "OUTPUT";
+    public static final String MOINS = "-";
+    public static final String MOD = "";
+    public static final String DIV = "/";
+    public static final String MUL = "*";
+    public static final String PLUS = "+";
+    public static final String LTE = "<=";
+    public static final String LT = "<";
+    public static final String GTE = ">=";
+    public static final String GT = ">";
+    public static final String EGAL = "=";
+    public static final String OR = "||";
+    public static final String AND = "&&";
+    public static final String NOT = "!";
+    public static final String SEMI = ";";
+
     Arbre(String val, Arbre fg, Arbre fd) {
         this.fd = fd;
         this.fg = fg;
